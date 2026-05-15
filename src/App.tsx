15ce37a9f1ddd94bd0ecefe8d891fcda44524508
Matheus-AlvesCS <matheusalves.macs@gmail.com>
2026-05-15 11:03:01 -0300

    feat(app): add initial App component structure and global styles

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./global.css"
+import styles from "./app.module.css"
+
+export function App() {
+  return (
+    <div className={styles.container}>
+      <main>
+        <h1>Mensagem teste</h1>
+        <button>Teste</button>
+      </main>
+    </div>
+  )
+}
