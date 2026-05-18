2462a44324558a37555f8eda785ff65ed5a3718c
Matheus-AlvesCS <matheusalves.macs@gmail.com>
2026-05-18 08:59:43 -0300

    feat(letter): add letter component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import "./global.css"
 
 import { Header } from "./components/Header"
 import { Tip } from "./components/Tip"
+import { Letter } from "./components/Letter"
 
 import styles from "./app.module.css"
 
@@ -15,6 +16,14 @@ export function App() {
       <main>
         <Header current={1} max={10} onRestart={restartGame} />
         <Tip />
+
+        <div className={styles.word}>
+          <Letter value="R" />
+          <Letter value="E" />
+          <Letter value="A" />
+          <Letter value="C" />
+          <Letter value="T" />
+        </div>
       </main>
     </div>
   )
